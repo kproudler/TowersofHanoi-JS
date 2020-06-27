c08c732f8a3a741dfb928c7596390e035234df3d
Kevin Proudler <kevinproudler@gmail.com>
2020-06-27 07:07:35 -0400

    initial

--- game.js
+++
@@ -1,9 +0,0 @@
-class Game {
-    run() {
-        /*
-        until a player has stacked all discs on one peg
-        get move from player
-        player makes move on board
-        */
-    }
-}
