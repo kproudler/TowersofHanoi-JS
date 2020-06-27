20e8f55069e503bc9989a4328be1abbe03bfa0fa
Kevin Proudler <kevinproudler@gmail.com>
2020-06-26 20:09:00 -0400

    initial commit

---
+++ game.js
@@ -0,0 +1,9 @@
+class Game {
+    run() {
+        /*
+        until a player has stacked all discs on one peg
+        get move from player
+        player makes move on board
+        */
+    }
+}
