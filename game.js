4c7d3c48651e991dd47292b188946dfe7e41fade
Kevin Proudler <kevinproudler@gmail.com>
2020-06-27 18:24:13 -0400

    fixed bug where invalid move ends program

--- game.js
+++ game.js
@@ -8,7 +8,11 @@ class Game {
         const startTower = this.towers[startTowerIdx];
         const endTower = this.towers[endTowerIdx];
 
-        if (startTower.length === 0) {
+        if (startTowerIdx < 0 || startTowerIdx > 2) {
+            return false;
+        } else if (endTowerIdx < 0 || endTowerIdx > 2) {
+            return false;
+        } else if (startTower.length === 0) {
             return false;
         } else if (endTower.length === 0) {
             return true
